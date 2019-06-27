99145a917f7d172a6e09f0745363492c2876f79e
Jean-Rémy Falleri <jr.falleri@gmail.com>
2019-06-27 10:36:52 +0200

    update plot to display each year

--- se_prices.r
+++ se_prices.r
@@ -2,6 +2,7 @@ library(reshape2)
 library(ggplot2)
 
 prices <- read.csv("se_prices.csv",colClasses=c("character","integer","integer","integer"))
+years <- seq(min(prices$year), max(prices$year))
 
 png(file="se_prices_reg.png")
 
@@ -12,6 +13,7 @@ ggplot(data = prices, aes(x=year, y=early_regular, colour=conference, shape=conf
 		xlab("Year") +
 		ylab("Price (EUR)") +
 		theme_bw() +
+		scale_x_discrete(limits=years) + 
 		scale_colour_discrete(name="Conference") +
 		scale_shape_discrete(name="Conference") +
 		guides(color=guide_legend(override.aes=list(fill=NA)))
@@ -27,6 +29,7 @@ ggplot(data = prices, aes(x=year, y=early_student, colour=conference, shape=conf
 		xlab("Year") +
 		ylab("Price (EUR)") +
 		theme_bw() +
+		scale_x_discrete(limits=years) + 
 		scale_colour_discrete(name="Conference") +
 		scale_shape_discrete(name="Conference") +
 		guides(color=guide_legend(override.aes=list(fill=NA)))

5005586a706042e26e58461496797e9730b949a0
Jean-Rémy Falleri <jr.falleri@gmail.com>
2021-11-23 15:43:13 +0100

    fix: factorize plot code

--- se_prices.r
+++ se_prices.r
@@ -1,42 +1,31 @@
 library(reshape2)
 library(ggplot2)
 
-prices <- read.csv("se_prices.csv",
+fees <- read.csv("se_prices.csv",
     colClasses = c("character", "integer", "integer", "integer"))
-years <- seq(min(prices$year), max(prices$year))
+years <- seq(min(fees$year), max(fees$year))
 
-png(file = "se_prices_reg.png", width = 960, height = 480)
+plot_fees <- function(fees, title, y_axis, filename) {
+    y_axis <- enquo(y_axis)
+    p <- ggplot(data = fees, aes(x = year, y = !! y_axis,
+            colour = conference, shape = conference)) +
+        geom_point() +
+        geom_line() +
+        ggtitle(title) +
+        xlab("Year") +
+        ylab("Price (EUR)") +
+        ylim(0, 1000) +
+        theme_bw() +
+        scale_x_discrete(limits = years) +
+        scale_colour_discrete(name = "Conference") +
+        scale_shape_discrete(name = "Conference") +
+        guides(color = guide_legend(override.aes = list(fill = NA)))
 
-ggplot(data = prices, aes(x = year, y = early_regular,
-        colour = conference, shape = conference)) +
-    geom_point() +
-    geom_line() +
-    ggtitle("Full conference price for a regular early registration.") +
-    xlab("Year") +
-    ylab("Price (EUR)") +
-    ylim(0, 1000) +
-    theme_bw() +
-    scale_x_discrete(limits = years) +
-    scale_colour_discrete(name = "Conference") +
-    scale_shape_discrete(name = "Conference") +
-    guides(color = guide_legend(override.aes = list(fill = NA)))
+    ggsave(filename, p, width = 6, height = 3)
+}
 
-dev.off()
+plot_fees(fees, "Full conferences' fees for a regular early registration.",
+    early_regular, "se_prices_reg.png")
 
-png(file = "se_prices_stu.png", width = 960, height = 480)
-
-ggplot(data = prices, aes(x = year, y = early_student,
-        colour = conference, shape = conference)) +
-    geom_point() +
-    geom_line() +
-    ggtitle("Full conference price for a student early registration.") +
-    xlab("Year") +
-    ylab("Price (EUR)") +
-    ylim(0, 1000) +
-    theme_bw() +
-    scale_x_discrete(limits = years) +
-    scale_colour_discrete(name = "Conference") +
-    scale_shape_discrete(name = "Conference") +
-    guides(color = guide_legend(override.aes = list(fill = NA)))
-
-dev.off()
+plot_fees(fees, "Full conferences' fees for a student early registration.",
+    early_student, "se_prices_stu.png")
